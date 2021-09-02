30fa1c7ce53dbc496fecab3079a920d4dc803d00
Bün Colak <bun.colak@motius.de>
2021-09-02 10:20:52 +0300

    register

--- api/register.js
+++ api/register.js
@@ -4,19 +4,25 @@ import 'isomorphic-fetch'
 const handler = async (req, res) => {
     const {data} = req.body.input;
     const headers = new Headers()
-    headers.set('Authorization', process.env.API_TOKEN || "")
+    headers.set('X-Hasura-Admin-Secret', process.env.API_TOKEN || "")
 
     const hashedPassword = await bcrypt.hash(data.password, 10)
 
     try {
-        await fetch(`${process.env.API_URL}/register`, {
+        const result = await fetch(`${process.env.API_URL}/register`, {
             body: JSON.stringify({...data, password: hashedPassword}),
             headers,
             method: 'post'
         })
-        res.json({
-            success: true
-        })
+        if (result.ok) {
+            return res.json({
+                success: true
+            })
+        } else {
+            return res.status(400).json({
+                message: 'Error registering.'
+            })
+        }
     } catch (e) {
         res.status(400).json({
             message: 'Error registering.'
